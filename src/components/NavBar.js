9b56287f40cb24e646f655d8964ef46003c6c213
stevebiczyk <stevebiczyk@gmail.com>
2023-06-07 21:16:22 +0000

    Added CSS styling to App and NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
-    return <Navbar expand="md" fixed="top">
+    return <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
