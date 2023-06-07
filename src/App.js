8c78702b40a445435ec1f971a078e21b8653f7be
stevebiczyk <stevebiczyk@gmail.com>
2023-06-07 17:45:25 +0000

    Created the nav bar and added Font Awesome icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Button } from 'react-bootstrap';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
