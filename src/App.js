3015e2eef2b7380ef8a0cad2d2b220197269efd5
stevebiczyk <stevebiczyk@gmail.com>
2023-06-10 14:10:30 +0000

    Creating Posts - Part 1

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
-
   return (
-
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
@@ -18,6 +16,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
