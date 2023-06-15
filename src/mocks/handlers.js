bfd24bc778ef02dbf70c836544bd216116f6cdad
stevebiczyk <stevebiczyk@gmail.com>
2023-06-15 20:11:50 +0000

    Setup to create React unit tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import rest from "msw";
+
+const baseURL = "https://drf-api-sb.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "steveb1",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/di4ba5hux/image/upload/v1/media/images/IMG_20210903_185807_830_xy8oeb",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
